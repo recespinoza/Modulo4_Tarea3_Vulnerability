6faa629fecaef898e8967a3f52d275f203455c7d
René Espinoza <recespinoza@gmail.com>
2020-10-11 00:42:28 -0400

    Tarea 3 vulnerabilidad

---
+++ src/test/java/control/CheckBox.java
@@ -0,0 +1,10 @@
+package control;
+
+import org.openqa.selenium.By;
+
+public class CheckBox extends  ControlSelenium {
+
+    public CheckBox(By locator, String myName) {
+        super(locator, myName);
+    }
+}
